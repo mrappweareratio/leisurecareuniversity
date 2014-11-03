88337d0717bc408f5df26234299905a101467db8
mrappweareratio <mrapp@weareratio.com>
2014-11-02 21:33:54 -0800

    rename from Portfolio to Leadership

--- wordpress/wp-content/themes/planus/planus/includes/portfolio-type.php
+++ wordpress/wp-content/themes/planus/planus/includes/portfolio-type.php
@@ -5,20 +5,20 @@ add_action('init', 'portfolio_register');
 function portfolio_register() {
 
 	$labels = array(
-		'name'               => _x( 'Portfolio items', 'post type general name', 'planuswp' ),
-		'singular_name'      => _x( 'Portfolio item', 'post type singular name', 'planuswp' ),
-		'menu_name'          => _x( 'Portfolio items', 'admin menu', 'planuswp' ),
-		'name_admin_bar'     => _x( 'Portfolio items', 'add new on admin bar', 'planuswp' ),
+		'name'               => _x( 'Leadership items', 'post type general name', 'planuswp' ),
+		'singular_name'      => _x( 'Leadership item', 'post type singular name', 'planuswp' ),
+		'menu_name'          => _x( 'Leadership items', 'admin menu', 'planuswp' ),
+		'name_admin_bar'     => _x( 'Leadership items', 'add new on admin bar', 'planuswp' ),
 		'add_new'            => _x( 'Add New', 'book', 'planuswp' ),
-		'add_new_item'       => __( 'Add New Portfolio item', 'planuswp' ),
-		'new_item'           => __( 'New Portfolio item', 'planuswp' ),
-		'edit_item'          => __( 'Edit Portfolio item', 'planuswp' ),
-		'view_item'          => __( 'View Portfolio item', 'planuswp' ),
-		'all_items'          => __( 'All Portfolio items', 'planuswp' ),
-		'search_items'       => __( 'Search Portfolios items', 'planuswp' ),
-		'parent_item_colon'  => __( 'Parent Portfolios item:', 'planuswp' ),
-		'not_found'          => __( 'No Portfolio items found.', 'planuswp' ),
-		'not_found_in_trash' => __( 'No Portfolios items found in Trash.', 'planuswp' ),
+		'add_new_item'       => __( 'Add New Leadership item', 'planuswp' ),
+		'new_item'           => __( 'New Leadership item', 'planuswp' ),
+		'edit_item'          => __( 'Edit Leadership item', 'planuswp' ),
+		'view_item'          => __( 'View Leadership item', 'planuswp' ),
+		'all_items'          => __( 'All Leadership items', 'planuswp' ),
+		'search_items'       => __( 'Search Leadership items', 'planuswp' ),
+		'parent_item_colon'  => __( 'Parent Leadership item:', 'planuswp' ),
+		'not_found'          => __( 'No Leadership items found.', 'planuswp' ),
+		'not_found_in_trash' => __( 'No Leadership items found in Trash.', 'planuswp' ),
 	);
 
 	$args = array(  
@@ -43,17 +43,17 @@ add_action( 'init', 'create_portfolio_taxonomies', 0 );
 function create_portfolio_taxonomies() {
 	// Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
-		'name'              => _x( 'Portfolio Category', 'taxonomy general name' ),
-		'singular_name'     => _x( 'Portfolio Category', 'taxonomy singular name' ),
-		'search_items'      => __( 'Search Portfolio Categories' ),
-		'all_items'         => __( 'All Portfolio Categories' ),
-		'parent_item'       => __( 'Parent Portfolio Category' ),
-		'parent_item_colon' => __( 'Parent Portfolio Category:' ),
-		'edit_item'         => __( 'Edit Portfolio Category' ),
-		'update_item'       => __( 'Update Portfolio Category' ),
-		'add_new_item'      => __( 'Add New Portfolio Category' ),
-		'new_item_name'     => __( 'New Portfolio Name' ),
-		'menu_name'         => __( 'Portfolio Categories' ),
+		'name'              => _x( 'Leadership Category', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Leadership Category', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Leadership Categories' ),
+		'all_items'         => __( 'All Leadership Categories' ),
+		'parent_item'       => __( 'Parent Leadership Category' ),
+		'parent_item_colon' => __( 'Parent Leadership Category:' ),
+		'edit_item'         => __( 'Edit Leadership Category' ),
+		'update_item'       => __( 'Update Leadership Category' ),
+		'add_new_item'      => __( 'Add New Leadership Category' ),
+		'new_item_name'     => __( 'New Leadership Name' ),
+		'menu_name'         => __( 'Leadership Categories' ),
 	);
 
 	$args = array(
